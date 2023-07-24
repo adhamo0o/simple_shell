b2f508e1f0e840cceffe19cbcdfb15ac912e1e92
adhamo0o <adhamelmesady414@gmail.com>
2023-07-24 15:44:46 -0700

    a

--- shell.h
+++ shell.h
@@ -1,15 +1,18 @@
-#ifndef SHELL_H 
- #define SHELL_H 
-  
- #include <stdio.h> 
- #include <stdlib.h> 
- #include <unistd.h> 
- #include <fcntl.h> 
- #include <sys/types.h> 
- #include <sys/wait.h> 
- #include <sys/stat.h> 
- #include <signal.h> 
- #include <errno.h> 
- #include <string.h>
+#ifndef SHELL_H
+#define SHELL_H
+
+#define buf_size 64
+#define delim " \t\r\n\a"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <signal.h>
+#include <errno.h>
+#include <string.h>
 
 #endif
