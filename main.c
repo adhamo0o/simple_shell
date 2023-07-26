fd4d8373fa324d5eb914bf4a46eb40af346b47e8
ahmed-mekky <27medsh3pan@gmail.com>
2023-07-27 00:12:33 +0300

    Hot Fix

--- main.c
+++ main.c
@@ -10,6 +10,23 @@
  */
 int main(int __attribute__((unused)) argc, char *argv[])
 {
-	shell_loop(argv[0]);
+	char *line;
+	char **words;
+	int status;
+	int n = 1, i;
+
+	do {
+		i = isatty(STDIN_FILENO);
+		if (i)
+			printf("($) ");
+		line = read_line(argv[0]);
+		words = cut_line(line, argv[0]);
+		status = execute_line(words, argv[0], n);
+		if (status == 2)
+			n++;
+
+		free(line);
+		free(words);
+	} while (status);
 	return (0);
 }
