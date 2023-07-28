990758b6d4d7bb87bea2366e812478df5a271338
ahmed-mekky <27medsh3pan@gmail.com>
2023-07-29 02:06:05 +0300

    Hot Fix

--- main.c
+++ main.c
@@ -10,9 +10,7 @@
  */
 int main(int argc, char **argv)
 {
-	FILE *input_F;
 	char *line = NULL, **args;
-	size_t bufsize = 0;
 	int status = 0;
 
 	(void)argc;
@@ -22,19 +20,14 @@ int main(int argc, char **argv)
 		loop();
 	else
 	{
-		input_F = stdin;
-		while (getline(&line, &bufsize, input_F) != -1)
-		{
-			line[strcspn(line, "\n")] = '\0';
-			args = cut_line(line);
-			status = execute_(args);
+		line = read_line();
+		args = cut_line(line);
+		status = execute_(args);
 
-			free(args);
-			if (status == 0)
-				break;
-		}
-
+		free(args);
 		free(line);
+		if (status == 0)
+			return(EXIT_SUCCESS);
 	}
 
 	if (status == 127)
