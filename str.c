2e3d5b7e9ab17e0f00ea753053b09b2d28aa0818
adhamo0o <adhamelmesady414@gmail.com>
2023-07-29 10:31:48 -0700

    some files

---
+++ str.c
@@ -0,0 +1,86 @@
+#include "shell.h"
+
+/**
+ * _strcpy_fun - copy string
+ * @dest: destination
+ * @src: source
+ *
+ * Return: ptr
+ */
+char *_strcpy_fun(char *dest, char *src)
+{
+	int i = 0;
+
+	if (dest == src || src == 0)
+		return (dest);
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = 0;
+	return (dest);
+}
+
+/**
+ * _strdup_fun - duplicate string
+ * @str: string
+ *
+ * Return: ptr
+ */
+char *_strdup_fun(const char *str)
+{
+	int length = 0;
+	char *ret;
+
+	if (str == NULL)
+		return (NULL);
+	while (*str++)
+		length++;
+	ret = malloc(sizeof(char) * (length + 1));
+	if (!ret)
+		return (NULL);
+	for (length++; length--;)
+		ret[length] = *--str;
+	return (ret);
+}
+
+/**
+ *_puts_fun - print input string
+ *@str: string to printed
+ *
+ * Return: Nothing
+ */
+void _puts_fun(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+}
+
+/**
+ * _putchar_fun - write character
+ * @c: character
+ *
+ * Return: On success 1
+ */
+int _putchar_fun(char c)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(1, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
